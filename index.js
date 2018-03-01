e3d996f57577e489c5e8605898546816ef5ae263
Hirevo <nipolom@live.fr>
2018-03-01 20:31:27 +0100

    Added Vehicle

--- index.js
+++ index.js
@@ -1,5 +1,48 @@
 
+class Vehicle {
+    constructor(maxTicks) {
+        this.maxTicks = maxTicks; //? Max number of ticks in simulation
+        this.agenda = Array(maxTicks).fill(false); //? Timeline of usage (true means "busy on a ride" and false means available)
+        this.rides = [];
+    }
+
+    canRide(range) {
+        if (range.some(elem => (elem < 0) || (elem >= this.agenda.length)))
+            return false;
+        return !this.agenda.slice(...range).some(elem => elem);
+    }
+
+    progRide(id, start, end) {
+        const distance = Math.abs(end[0] - starŧ[0]) + Math.abs(end[1] - starŧ[1])
+        if (distance.some(elem => (elem < 0) || (elem >= this.agenda.length)))
+            return;
+        this.agenda.fill(true, ...distance);
+        this.rides.push({ id, distance });
+    }
+
+    isBusy(curTick) {
+        return this.agenda[curTick];
+    }
+
+    isFree(curTick) {
+        return !this.agenda[curTick];
+    }
+    
+    getRides() {
+        return this.rides.sort((a, b) => a.range[0] > b.range[0]);
+    }
+}
+
 const parser = require("./parse.js");
 const util = require("util");
 
-console.log(util.inspect(parser(process.argv[2]), { depth: null }));
+const input = parser(process.argv[2]);
+
+// console.log(input);
+
+const vehicles = Array(input.vehicles).map(elem => new Vehicle());
+
+input.rides.sort((a, b) => a.start[0] > b.start[0]);
+
+console.log(input.rides);
+
